0f15030127004bede8d521a9665254321232c438
Isabelle Titaw <46397217+IsaTitaw@users.noreply.github.com>
2020-02-18 08:42:38 +0100

    update index

--- index.php
+++ index.php
@@ -8,14 +8,32 @@ foreach (glob(__DIR__."/controllers/*.php") as $filename)
     include_once $filename;
 }
 
-
+session_start();
 $request = explode('/', $_SERVER['REQUEST_URI'])[3];
-
 
 switch ($request) {
     case '' :
         $controller = new HomeController();
         break;
+
+    case 'product':
+        $id = explode('/', $_SERVER['REQUEST_URI'])[4];
+        $controller = new ProductController($id);
+        break;
+
+    case 'search':
+        $controller = new SearchController();
+        break;
+
+    case 'login':
+        $controller = new LoginController();
+        break;
+
+    case 'logout':
+        session_unset();
+        $controller = new HomeController();
+        break;
+
     default:
         http_response_code(404);
         //        require __DIR__ . '/views/404.php';
